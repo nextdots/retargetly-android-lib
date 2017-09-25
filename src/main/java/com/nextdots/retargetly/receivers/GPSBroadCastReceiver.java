0ef0725856080d2aa4e9a606aa8f1f0fd53a7874
Luis Tundisi <luistundisi@gmail.com>
2017-09-25 17:25:59 -0400

    test forceGPS

--- src/main/java/com/nextdots/retargetly/receivers/GPSBroadCastReceiver.java
+++ src/main/java/com/nextdots/retargetly/receivers/GPSBroadCastReceiver.java
@@ -1,25 +1,70 @@
 package com.nextdots.retargetly.receivers;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import static com.nextdots.retargetly.api.ApiConstanst.TAG;
 
-public class GPSBroadCastReceiver extends BroadcastReceiver
-{
+public class GPSBroadCastReceiver extends BroadcastReceiver implements LocationListener {
+
+    private Criteria criteria;
+    private String provider;
+
     @Override
-    public void onReceive(Context context, Intent intent )
-    {
-        final LocationManager manager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
-        if (manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-            Log.d(TAG,"GPS ENABLE");
+    public void onReceive(Context context, Intent intent) {
+        final LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            Log.d(TAG, "GPS ENABLE");
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            provider = manager.getBestProvider(criteria, true);
+
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1,
+                        0, this);
+                setMostRecentLocation(manager.getLastKnownLocation(provider));
+            }
         }
         else
         {
             Log.d(TAG,"GPS DISABLE");
         }
+    }
+
+    private void setMostRecentLocation(Location lastKnownLocation) {
+        Log.d(TAG, "Altitude "+lastKnownLocation.getAltitude()+"");
+        Log.d(TAG, "latitude "+lastKnownLocation.getLatitude()+"");
+        Log.d(TAG, "longitude "+lastKnownLocation.getLongitude()+"");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
